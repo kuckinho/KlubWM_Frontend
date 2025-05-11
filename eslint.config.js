73a58f48ee9f0e7e5a5405b7763f5de17947e12a
jmateyka <s0584167@htw-berlin.de>
2025-05-11 12:32:45 +0200

    Anpassung in package.json und eslint.config.js um 'process' zu definieren (node)

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default defineConfig([
   {
@@ -17,16 +17,17 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node, // Fügt die Node.js Umgebung hinzu
       },
     },
   },
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
-])
+]);
