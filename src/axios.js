c18cf2fba86152871f12d777e85016ddd8d506f5
jmateyka <s0584167@htw-berlin.de>
2025-05-11 13:02:46 +0200

    Anpassungen in axios.js wegen Anpassung Umgebungsvariable

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITEAPIURL, // verwendet die Umgebungsvariable
+  baseURL: import.meta.env.VITE_APP_BACKEND_BASE_URL, // Umgebungsvariable
   headers: {
     'Content-Type': 'application/json',
   },
