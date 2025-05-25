bb73e5149422afdf65b5c2e03950c34360e8ffdc
kuckinho <j-kuck@gmx.de>
2025-05-25 22:02:46 +0200

    Anpassungen CSS

--- src/components/MatchCalculator.vue
+++ src/components/MatchCalculator.vue
@@ -163,29 +163,21 @@ function updateGroupStats() {
 
 async function saveAllMatches() {
   try {
-    // Prüfe nur die Matches, deren Ergebnisse beide nicht null sind
-    const matchUpdates = matches.value
-      .filter(match => match.homeScore !== null && match.visitorScore !== null)
-      .map(match => ({
-        id: match.id,
-        homeScore: match.homeScore,
-        visitorScore: match.visitorScore,
-      }));
+    const matchUpdates = matches.value.map(match => ({
+      id: match.id,
+      homeScore: match.homeScore,
+      visitorScore: match.visitorScore,
+    }));
 
-    // Nur speichern, wenn es Spiele mit Ergebnissen gibt
-    if (matchUpdates.length > 0) {
-      await apiClient.put('/matches/batch', matchUpdates);
-      // Nach dem Speichern die Gruppen neu laden, um Statistiken basierend auf gespeicherten, echten Ergebnissen zu aktualisieren
-      await loadGroups();
-    } else {
-      console.log("Keine gültigen Ergebnisse zum Speichern.");
-    }
+    await apiClient.put('/matches/batch', matchUpdates);
+
+    // Nach dem Speichern die Gruppen neu laden
+    await loadGroups();
 
   } catch (error) {
     console.error("Error saving matches:", error);
   }
 }
-
 
 function resetAllMatches() {
   matches.value.forEach(match => {
